a304b3e23ebf2a004b1e930ea630291ada85a767
luis.hk <kul@hss.edu>
2023-09-04 11:29:27 -0400

    configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'primaryGreen': '#3cb54b',
+                'primaryOrange': '#f6891f',
+                'primaryYellow': '#f7c61a',
+                'primary': '#464646'
+            }
+        },
+    },
+    plugins: [],
+};
 
