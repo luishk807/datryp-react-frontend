8dce63a90223a4f06a4c08039d80f252b31a6958
luis.hk <kul@hss.edu>
2024-09-29 08:59:27 -0400

    trip detail

---
+++ src/components/Sections/TripDetail/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+import PropTypes from 'prop-types';
+
+export const TripDetail = (props) => {
+    return(
+        <div>
+            Ttest
+        </div>
+    );
+};
+
+export default TripDetail;
